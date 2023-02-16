45e95dbcf7718e7bf4d776e1a4f7cdbebbb6520e
Tiago Tresoldi <tresoldi@gmail.com>
2023-02-16 19:16:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,19 @@ LOCAL_PATH = pathlib.Path(__file__).parent
 README_FILE = (LOCAL_PATH / "README.md").read_text()
 
 # Load requirements, so they are listed in a single place
-with open("requirements.txt") as fp:
+with open(LOCAL_PATH / "requirements.txt") as fp:
     install_requires = [dep.strip() for dep in fp.readlines()]
 
 # This call to setup() does all the work
 setup(
     author="Tiago Tresoldi",
     author_email="tiago.tresoldi@lingfil.uu.se",
-    classifiers=[        "License :: OSI Approved :: MIT License",        "Operating System :: OS Independent",        "Programming Language :: Python :: 3",        "Topic :: Software Development :: Libraries",],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
     description="Python libary for ngram collection and frequency smoothing",
     entry_points={"console_scripts": ["lpngram=lpngram.__main__:main"]},
     include_package_data=True,
@@ -33,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=[],
     url="https://github.com/tresoldi/lpngram",
-    version="0.2", # sync with __init__.py
+    version="0.2",  # sync with __init__.py
     zip_safe=False,
 )
